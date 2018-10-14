dec66ed1907a8a6a68f2853cf88b0321646f3d18
joshuaburton2 <43986511+joshuaburton2@users.noreply.github.com>
2018-10-14 14:20:22 +1000

    SingleDoorAndMaterials

---
+++ IGB321 Assignment 3/Assets/Josh/Scripts/DoorOpen.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+    public GameObject doorPositive;
+    public GameObject doorNegative;
+
+    public bool open = false;
+
+    private Animation doorAnimPositive;
+    private Animation doorAnimNegative;
+
+    private void Start()
+    {
+        //Retrieves the animations for both doors if they are assigned. Script works for both single and double doors
+
+        //Retrieve positive 90 degree animation
+        if (doorPositive != null)
+        {
+            doorAnimPositive = doorPositive.GetComponent<Animation>();
+        }
+        //Retrieve negative 90 degree animation
+        if (doorNegative != null)
+        {
+            doorAnimNegative = doorNegative.GetComponent<Animation>();
+        }
+        
+    }
+
+    public void DoorInteract()
+    {
+        if (!doorAnimPositive.isPlaying || !doorAnimNegative.isPlaying)
+        {
+            if (!open)
+            {
+                doorAnimPositive.Play("PositiveDoorOpen");
+                doorAnimNegative.Play("NegativeDoorOpen");
+                open = true;
+            }
+            else
+            {
+                doorAnimPositive.Play("PositiveDoorClose");
+                doorAnimNegative.Play("NegativeDoorClose");
+                open = false;
+            }
+        }
+    }
+}
